65c8928f4210158ffd4ffe15047110cf5ec22446
Ivan Stanojevic <ivanstan@gmail.com>
2019-12-24 21:54:20 +0100

    add batch to csv reader

--- src/Service/CsvReader.php
+++ src/Service/CsvReader.php
@@ -4,12 +4,21 @@ namespace App\Service;
 
 class CsvReader
 {
+    private string $filename;
     private $handle;
 
     public function __construct(string $filename, string $mode = null)
     {
+        if (!file_exists($filename)) {
+            throw new \RuntimeException(\sprintf('File "%s" not found.', $filename));
+        }
+
         $this->filename = $filename;
         $this->handle = fopen($filename, $mode ?? 'rb');
+
+        if ($this->handle === false) {
+            throw new \RuntimeException(\sprintf('Unable to open "%s" file.', $filename));
+        }
     }
 
     public function get(): \Generator
@@ -17,6 +26,24 @@ class CsvReader
         while (($data = fgetcsv($this->handle)) !== false) {
             yield $data;
         }
+    }
+
+    public function batch(int $size): ?\Generator
+    {
+        $chunk = [];
+
+        $counter = 1;
+        foreach ($this->get() as $line) {
+            $chunk[] = $line;
+            $counter++;
+
+            if (($counter % $size) === 0) {
+                yield $chunk;
+                $chunk = [];
+            }
+        }
+
+        yield $chunk;
     }
 
     public function count(): int
