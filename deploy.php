7d90b5cf17334da9b8e6c0d2d379f29bb1663a26
Ivan Stanojevic <ivanstan@gmail.com>
2020-01-02 22:12:24 +0100

    setup tests before deploy

--- deploy.php
+++ deploy.php
@@ -1,36 +1,18 @@
-<?php
+<?php /** @noinspection ALL */
 
 namespace Deployer;
 
 require 'recipe/symfony.php';
 
-// Project repository
 set('repository', 'https://github.com/ivanstan/tle-api');
-
-// [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true);
 set('bin_dir', 'bin');
 set('http_user', 'glutenfr');
 set('writable_mode', 'chmod');
 
-// Shared files/dirs between deploys 
-add(
-    'shared_files',
-    [
-        '.env'
-    ]
-);
-add(
-    'shared_dirs',
-    [
-        'var'
-    ]
-);
-
-// Writable dirs by web server 
+add('shared_files', ['.env']);
+add('shared_dirs', ['var']);
 add('writable_dirs', []);
-
-// Hosts
 
 host('data.ivanstanojevic.me')
     ->user('glutenfr')
@@ -38,35 +20,30 @@ host('data.ivanstanojevic.me')
     ->stage('production')
     ->set('deploy_path', '~/projects/tle.ivanstanojevic.me');
 
-// Tasks
+task('test', function () {
+    set('symfony_env', 'dev');
+    runLocally('bin/phpunit');
+});
 
-task(
-    'deploy',
-    [
-        'deploy:info',
-        'deploy:prepare',
-        'deploy:lock',
-        'deploy:release',
-        'deploy:update_code',
-        'deploy:clear_paths',
-        'deploy:create_cache_dir',
-        'deploy:shared',
-        'deploy:assets',
-        'deploy:vendors',
-        'deploy:cache:clear',
-        'deploy:cache:warmup',
-        'deploy:writable',
-        'database:migrate',
-        'deploy:symlink',
-        'deploy:unlock',
-        'cleanup',
-    ]
-);
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:clear_paths',
+    'deploy:create_cache_dir',
+    'deploy:shared',
+    'deploy:assets',
+    'deploy:vendors',
+    'deploy:cache:clear',
+    'deploy:cache:warmup',
+    'deploy:writable',
+    'database:migrate',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+]);
 
-// [Optional] if deploy fails automatically unlock.
+before('deploy', 'test');
 after('deploy:failed', 'deploy:unlock');
-
-// Migrate database before symlink new release.
-
-before('deploy:symlink', 'database:migrate');
-
