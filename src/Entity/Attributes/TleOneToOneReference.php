3da8daa883cd17ab68ef5169b6c758bf209854dd
Ivan Stanojevic <ivanstan@gmail.com>
2021-04-01 15:03:20 +0200

    add tle subscriber

---
+++ src/Entity/Attributes/TleOneToOneReference.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Entity\Attributes;
+
+use App\Entity\Tle;
+use Doctrine\ORM\Mapping as ORM;
+
+trait TleOneToOneReference
+{
+    /**
+     * @ORM\Id
+     * @ORM\OneToOne(targetEntity="Tle")
+     * @ORM\JoinColumn(name="tle_id", referencedColumnName="id", nullable=false)
+     */
+    protected Tle $tle;
+
+    public function getTle(): Tle
+    {
+        return $this->tle;
+    }
+
+    public function setTle(Tle $tle): void
+    {
+        $this->tle = $tle;
+    }
+}
