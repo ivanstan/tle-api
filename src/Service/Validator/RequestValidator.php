f3133b26989d889a9962c2de2e1925c63ff5bdbe
Ivan Stanojevic <ivanstan@gmail.com>
2021-04-02 11:43:21 +0200

    filter validator added

--- src/Service/Validator/RequestValidator.php
+++ src/Service/Validator/RequestValidator.php
@@ -78,4 +78,63 @@ trait RequestValidator
 
         return $this;
     }
+
+    protected function assertFilter(Request $request, array $filters): array
+    {
+        $result = [];
+
+        foreach ($filters as $filter => $type) {
+            $values = $request->get($filter);
+
+            foreach ($values as $operator => $value) {
+                $operator = $this->assertOperator($operator, $type, $filter);
+                $value = $this->assertValue($value, $type, $filter);
+
+                $result[$filter][$operator] = $value;
+            }
+        }
+
+        return $result;
+    }
+
+    protected function assertOperator(string $operator, string $type, string $filter): ?string
+    {
+        if ($type === self::FILTER_TYPE_FLOAT) {
+            $operators = self::FILTER_FLOAT_OPERATORS[self::FILTER_TYPE_FLOAT];
+            if (!array_key_exists($operator, $operators)) {
+                throw new BadRequestHttpException(
+                    \sprintf(
+                        'Operator for filter \'%s\' should be one of the following %s, \'%s\' provided',
+                        $filter,
+                        implode(', ', array_keys($operators)),
+                        $operator
+                    )
+                );
+            }
+
+            return $operators[$operator];
+        }
+
+        return null;
+    }
+
+    /**
+     * @noinspection CallableParameterUseCaseInTypeContextInspection
+     */
+    protected function assertValue(string $value, string $type, string $filter): mixed
+    {
+        if ($type === self::FILTER_TYPE_FLOAT) {
+            $value = (float)$value;
+
+            if (!is_float($value)) {
+                throw new BadRequestHttpException(
+                    \sprintf('Filter %s value should be %s', $filter, $type)
+                );
+            }
+
+            return $value;
+        }
+
+        return null;
+    }
 }
