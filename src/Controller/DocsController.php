d904569db40f9bd55de95ac4d0b4c287f3549ddb
Ivan Stanojevic <ivanstan@gmail.com>
2021-06-05 13:40:32 +0200

    upgrade symfony 5.3

--- src/Controller/DocsController.php
+++ src/Controller/DocsController.php
@@ -12,26 +12,19 @@ final class DocsController extends AbstractController
 {
     use FileSystemAwareTrait;
 
-    /**
-     * @Route("/", name="tle_home")
-     * @Route("/api/tle/docs", name="app_api_docs")
-     */
+    #[Route("/", name: "tle_home")]
+    #[Route('/api/tle/docs', name: "app_api_docs")]
     public function docs(): Response
     {
         return new Response(file_get_contents($this->getProjectDir() . '/public/index.html'));
     }
 
-    /**
-     * @Route("/api/tle.json", name="app_api_docs_json")
-     * @throws \JsonException
-     */
+    #[Route("/api/tle.json", name: "app_api_docs_json")]
     public function getJson(): JsonResponse
     {
         $path = $this->getProjectDir() . '/config/custom/tle.json';
 
         $docs = json_decode(file_get_contents($path), true, 512, JSON_THROW_ON_ERROR);
-
-//        unset($docs['paths']['/api/tle/{id}/propagate']);
 
         return new JsonResponse($docs, Response::HTTP_OK, ['Access-Control-Allow-Origin' => '*',]);
     }
