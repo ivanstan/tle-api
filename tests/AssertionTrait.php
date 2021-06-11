d0d4a73a8754f0699e79aa29fe97a76683a53c58
Ivan Stanojevic <ivanstan@gmail.com>
2021-06-11 22:16:40 +0200

    update tests

--- tests/AssertionTrait.php
+++ tests/AssertionTrait.php
@@ -6,7 +6,7 @@ use App\Entity\Tle;
 
 trait AssertionTrait
 {
-    public function assertTle(Tle $tle, array $response, bool $extra = false): void
+    public function assertTle(Tle $tle, array $response): void
     {
         $model = new \Ivanstan\Tle\Model\Tle($tle->getLine1(), $tle->getLine2(), $tle->getName());
 
@@ -15,7 +15,7 @@ trait AssertionTrait
         self::assertEquals('TleModel', $response['@type']);
         self::assertEquals($tle->getId(), $response['satelliteId']);
         self::assertEquals($tle->getName(), $response['name']);
-        self::assertEquals($model->getDate(), $response['date']);
+        self::assertEquals($model->epochDateTime()->format(\DateTimeInterface::ATOM), $response['date']);
         self::assertEquals($tle->getLine1(), $response['line1']);
         self::assertEquals($tle->getLine2(), $response['line2']);
     }
--- tests/Controller/PropagateControllerTest.php
+++ tests/Controller/PropagateControllerTest.php
@@ -16,7 +16,7 @@ class PropagateControllerTest extends AbstractWebTestCase
 
         $response = $this->toArray($response);
 
-        self::assertEquals($response['response']['message'], 'Unable to find record with id 0');
+        self::assertEquals('Unable to find record with id 0', $response['response']['message']);
     }
 
     public function testPropagateSGP4(): void
@@ -34,34 +34,34 @@ class PropagateControllerTest extends AbstractWebTestCase
 
         $response = $this->toArray($response);
 
-        self::assertEquals($response['@context'], 'https://www.w3.org/ns/hydra/context.jsonld');
-        self::assertEquals($response['@id'], 'http://localhost/api/tle/43550/propagate?date=2021-04-26T17:49:45%2B02:00');
-        self::assertEquals($response['@type'], 'SatellitePropagationResult');
+        self::assertEquals('https://www.w3.org/ns/hydra/context.jsonld', $response['@context']);
+        self::assertEquals('http://localhost/api/tle/43550/propagate?date=2021-04-26T17:49:45%2B02:00', $response['@id']);
+        self::assertEquals('SatellitePropagationResult', $response['@type']);
 
         self::assertArrayHasKey('tle', $response);
 
-        self::assertEquals($response['algorithm'], 'SGP4');
+        self::assertEquals('SGP4', $response['algorithm']);
 
-        self::assertEquals($response['vector']['reference_frame'], 'ECI');
+        self::assertEquals('ECI', $response['vector']['reference_frame']);
 
-        self::assertEquals($response['vector']['position']['x'], 3731.3677738358);
-        self::assertEquals($response['vector']['position']['y'], -3929.0247024138);
-        self::assertEquals($response['vector']['position']['z'], -3820.6175474185);
-        self::assertEquals($response['vector']['position']['r'], 6630.0421581948);
-        self::assertEquals($response['vector']['position']['unit'], 'km');
+        self::assertEquals(3731.3677738358, $response['vector']['position']['x']);
+        self::assertEquals(-3929.0247024138, $response['vector']['position']['y']);
+        self::assertEquals(-3820.6175474185, $response['vector']['position']['z']);
+        self::assertEquals(6630.0421581948, $response['vector']['position']['r']);
+        self::assertEquals('km', $response['vector']['position']['unit']);
 
-        self::assertEquals($response['vector']['velocity']['x'], 2.2692661551689);
-        self::assertEquals($response['vector']['velocity']['y'], 6.1586427245624);
-        self::assertEquals($response['vector']['velocity']['z'], -4.1239106928913);
-        self::assertEquals($response['vector']['velocity']['r'], 7.7514571852487);
-        self::assertEquals($response['vector']['velocity']['unit'], 'km/s');
+        self::assertEquals(2.2692661551689, $response['vector']['velocity']['x']);
+        self::assertEquals(6.1586427245624, $response['vector']['velocity']['y']);
+        self::assertEquals(-4.1239106928913, $response['vector']['velocity']['z']);
+        self::assertEquals(7.7514571852487, $response['vector']['velocity']['r']);
+        self::assertEquals('km/s', $response['vector']['velocity']['unit']);
 
-        self::assertEquals($response['geodetic']['latitude'], -35.362152001955);
-        self::assertEquals($response['geodetic']['longitude'], 221.21616992358);
-        self::assertEquals($response['geodetic']['altitude'], 259.03105001661);
+        self::assertEquals(-35.362152001955, $response['geodetic']['latitude']);
+        self::assertEquals(221.21616992358, $response['geodetic']['longitude']);
+        self::assertEquals(259.03105001661, $response['geodetic']['altitude']);
 
-        self::assertEquals($response['parameters']['satelliteId'], 43550);
-        self::assertEquals($response['parameters']['date'], '2021-04-26T17:49:45+02:00');
+        self::assertEquals(43550, $response['parameters']['satelliteId']);
+        self::assertEquals('2021-04-26T17:49:45+02:00', $response['parameters']['date']);
     }
 
     public function testPropagateSDP4(): void
@@ -79,33 +79,33 @@ class PropagateControllerTest extends AbstractWebTestCase
 
         $response = $this->toArray($response);
 
-        self::assertEquals($response['@context'], 'https://www.w3.org/ns/hydra/context.jsonld');
-        self::assertEquals($response['@id'], 'http://localhost/api/tle/22049/propagate?date=2021-04-26T17:49:45%2B02:00');
-        self::assertEquals($response['@type'], 'SatellitePropagationResult');
+        self::assertEquals('https://www.w3.org/ns/hydra/context.jsonld', $response['@context']);
+        self::assertEquals('http://localhost/api/tle/22049/propagate?date=2021-04-26T17:49:45%2B02:00', $response['@id']);
+        self::assertEquals('SatellitePropagationResult', $response['@type']);
 
         self::assertArrayHasKey('tle', $response);
 
-        self::assertEquals($response['algorithm'], 'SDP4');
+        self::assertEquals('SDP4', $response['algorithm']);
 
-        self::assertEquals($response['vector']['reference_frame'], 'ECI');
+        self::assertEquals('ECI', $response['vector']['reference_frame']);
 
-        self::assertEquals($response['vector']['position']['x'], 142825.54086031896);
-        self::assertEquals($response['vector']['position']['y'], 133973.34798843606);
-        self::assertEquals($response['vector']['position']['z'], 1303.6185230048);
-        self::assertEquals($response['vector']['position']['r'], 195830.7751976863);
-        self::assertEquals($response['vector']['position']['unit'], 'km');
+        self::assertEquals(142825.54086031896, $response['vector']['position']['x']);
+        self::assertEquals(133973.34798843606, $response['vector']['position']['y']);
+        self::assertEquals(1303.6185230048, $response['vector']['position']['z']);
+        self::assertEquals(195830.7751976863, $response['vector']['position']['r']);
+        self::assertEquals('km', $response['vector']['position']['unit']);
 
-        self::assertEquals($response['vector']['velocity']['x'], -0.51310326492624);
-        self::assertEquals($response['vector']['velocity']['y'], 0.5491989174236);
-        self::assertEquals($response['vector']['velocity']['z'], 0.60190735910381);
-        self::assertEquals($response['vector']['velocity']['r'], 0.96290543685273);
-        self::assertEquals($response['vector']['velocity']['unit'], 'km/s');
+        self::assertEquals(-0.51310326492624, $response['vector']['velocity']['x']);
+        self::assertEquals(0.5491989174236, $response['vector']['velocity']['y']);
+        self::assertEquals(0.60190735910381, $response['vector']['velocity']['z']);
+        self::assertEquals(0.96290543685273, $response['vector']['velocity']['r']);
+        self::assertEquals('km/s', $response['vector']['velocity']['unit']);
 
-        self::assertEquals($response['geodetic']['latitude'], 0.38149611267203);
-        self::assertEquals($response['geodetic']['longitude'], 310.86248495862);
-        self::assertEquals($response['geodetic']['altitude'], 189452.64114393186);
+        self::assertEquals(0.38149611267203, $response['geodetic']['latitude']);
+        self::assertEquals(310.86248495862, $response['geodetic']['longitude']);
+        self::assertEquals(189452.64114393186, $response['geodetic']['altitude']);
 
-        self::assertEquals($response['parameters']['satelliteId'], 22049);
-        self::assertEquals($response['parameters']['date'], '2021-04-26T17:49:45+02:00');
+        self::assertEquals(22049, $response['parameters']['satelliteId']);
+        self::assertEquals('2021-04-26T17:49:45+02:00', $response['parameters']['date']);
     }
 }
