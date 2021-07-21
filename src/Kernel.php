8b16d57e168f0e0f1cf9fccffe81fcb205f6f6ee
Ivan Stanojevic <ivanstan@gmail.com>
2021-07-21 10:03:07 +0200

    rename config to etc

--- src/Kernel.php
+++ src/Kernel.php
@@ -9,22 +9,15 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
-/**
- * @codeCoverageIgnore
- */
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
     private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
-    /**
-     * @noinspection UsingInclusionReturnValueInspection
-     * @noinspection PhpIncludeInspection
-     */
     public function registerBundles(): iterable
     {
-        $contents = require $this->getProjectDir().'/etc/bundles.php';
+        $contents = require $this->getConfigDir() . '/bundles.php';
         foreach ($contents as $class => $envs) {
             if ($envs[$this->environment] ?? $envs['all'] ?? false) {
                 yield new $class();
@@ -32,31 +25,37 @@ class Kernel extends BaseKernel
         }
     }
 
-    /** @noinspection PhpMissingParentCallCommonInspection */
     public function getProjectDir(): string
     {
         return \dirname(__DIR__);
     }
 
-    /** @throws \Exception */
-    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
+    public function getConfigDir(): string
     {
-        $container->addResource(new FileResource($this->getProjectDir().'/etc/bundles.php'));
-        $container->setParameter('container.dumper.inline_class_loader', \PHP_VERSION_ID < 70400);
-        $container->setParameter('container.dumper.inline_factories', true);
-        $confDir = $this->getProjectDir().'/etc';
-
-        $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{packages}/'.$this->environment.'/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{parameters}'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
+        return $this->getProjectDir() . '/etc';
     }
 
-    /** @throws \Exception */
+    /**
+     * @throws \Exception
+     */
+    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
+    {
+        $confDir = $this->getConfigDir();
+
+        $container->addResource(new FileResource($confDir . '/bundles.php'));
+        $container->setParameter('container.dumper.inline_class_loader', \PHP_VERSION_ID < 70400);
+        $container->setParameter('container.dumper.inline_factories', true);
+
+        $loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{packages}/' . $this->environment . '/*' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{parameters}' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{services}' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{services}_' . $this->environment . self::CONFIG_EXTS, 'glob');
+    }
+
     protected function configureRoutes(RoutingConfigurator  $routes): void
     {
-        $confDir = $this->getProjectDir().'/etc';
+        $confDir = $this->getConfigDir();
 
         $routes->import($confDir.'/{routes}/'.$this->environment.'/*'.self::CONFIG_EXTS);
         $routes->import($confDir.'/{routes}/*'.self::CONFIG_EXTS);
