5e25e64b5363b868e0456efd7752e883ac9a462a
Ivan Stanojevic <ivanstan@gmail.com>
2021-08-04 20:29:15 +0200

    decouple pagination from repository

--- src/ViewModel/Model/PaginationCollection.php
+++ src/ViewModel/Model/PaginationCollection.php
@@ -2,29 +2,45 @@
 
 namespace App\ViewModel\Model;
 
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\QueryBuilder;
+
 class PaginationCollection
 {
-    private array $collection = [];
-
-    private int $total = 0;
-
-    public function getCollection(): array
+    public function __construct(protected QueryBuilder $builder)
     {
-        return $this->collection;
     }
 
-    public function setCollection(array $collection): void
-    {
-        $this->collection = $collection;
+    public function getCollection(
+        int $pageSize,
+        int $offset
+    ): array {
+        $builder = clone $this->builder;
+
+        $builder->setMaxResults($pageSize);
+        $builder->setFirstResult($offset);
+
+        return $builder->getQuery()->getResult();
     }
 
     public function getTotal(): int
     {
-        return $this->total;
-    }
+        $builder = clone $this->builder;
 
-    public function setTotal(int $total): void
-    {
-        $this->total = $total;
+        $alias = $builder->getRootAliases()[0] ?? null;
+        $entity = $builder->getRootEntities()[0] ?? null;
+
+        $meta = $builder->getEntityManager()->getClassMetadata($entity);
+        $identifier = $meta->identifier[0] ?? null;
+
+        $builder->select("COUNT($alias.$identifier)");
+
+        try {
+            return $builder->getQuery()->getSingleScalarResult();
+        } catch (NonUniqueResultException) {
+            $result = array_map(static fn($item) => (int)$item, $builder->getQuery()->getScalarResult());
+
+            return array_sum($result);
+        }
     }
 }
