5196b02e39c612536fedb0bebb77bff12b2b801f
Ivan Stanojevic <ivanstan@gmail.com>
2021-11-16 16:58:35 +0100

    add validator config

--- src/Request/CollectionRequest.php
+++ src/Request/CollectionRequest.php
@@ -6,13 +6,26 @@ use App\Enum\SortDirectionEnum;
 
 class CollectionRequest extends AbstractRequest
 {
+    /** @var int ToDo: use late static binding */
     public const PAGE_MAX_SIZE = 100;
     public const PAGE_PARAM = 'page';
     public const PAGE_SIZE = 20;
     public const PAGE_SIZE_PARAM = 'page-size';
-    public const SEARCH_PARAM = 'search';
+
     public const SORT_DIR_PARAM = 'sort-dir';
     public const SORT_PARAM = 'sort';
+
+    public const SEARCH_PARAM = 'search';
+
+    public function getPage(): int
+    {
+        return (int)$this->get(self::PAGE_PARAM, 1);
+    }
+
+    public function getPageSize(int $maxPageSize = self::PAGE_MAX_SIZE): int
+    {
+        return (int)min($this->get(self::PAGE_SIZE_PARAM, self::PAGE_SIZE), $maxPageSize);
+    }
 
     public function getSortDirection(): string
     {
@@ -27,15 +40,5 @@ class CollectionRequest extends AbstractRequest
     public function getSearch(): ?string
     {
         return $this->get(self::SEARCH_PARAM);
-    }
-
-    public function getPage(): int
-    {
-        return (int)$this->get(self::PAGE_PARAM, 1);
-    }
-
-    public function getPageSize(int $maxPageSize = self::PAGE_MAX_SIZE): int
-    {
-        return (int)min($this->get(self::PAGE_SIZE_PARAM, self::PAGE_SIZE), $maxPageSize);
     }
 }
--- src/Service/CustomRequestResolver.php
+++ src/Service/CustomRequestResolver.php
@@ -6,9 +6,15 @@ use App\Request\AbstractRequest;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class CustomRequestResolver implements ArgumentValueResolverInterface
 {
+    public function __construct(protected ValidatorInterface $validator)
+    {
+    }
+
     public function supports(Request $request, ArgumentMetadata $argument): bool
     {
         return Request::class === $argument->getType() || is_subclass_of($argument->getType(), Request::class);
@@ -19,6 +25,21 @@ class CustomRequestResolver implements ArgumentValueResolverInterface
         /** @var AbstractRequest $customRequest */
         $customRequest = forward_static_call([$argument->getType(), 'createFromGlobals']);
         $customRequest->attributes = $request->attributes;
+
+        if (method_exists($customRequest, 'validate')) {
+            $violations = $customRequest->validate($this->validator);
+
+            /**
+             * ToDo: Messages should be combined and well formatted.
+             */
+            if ($violations !== null && $violations->has(0)) {
+                $violation = $violations->get(0);
+
+                $message = $violation->getPropertyPath() . ' ' . $violation->getMessage();
+
+                throw new BadRequestHttpException($message);
+            }
+        }
 
         yield $customRequest;
     }
