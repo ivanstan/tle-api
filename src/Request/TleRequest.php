90f64a2ba47240eb2b608976cd2e9e4e7e0fb335
Ivan Stanojevic <ivanstan@gmail.com>
2022-02-15 17:11:34 +0100

    add cache to docs

--- src/Request/TleRequest.php
+++ src/Request/TleRequest.php
@@ -2,16 +2,26 @@
 
 namespace App\Request;
 
+use App\Repository\TleRepository;
 use Ivanstan\SymfonySupport\Request\AbstractRequest;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Contracts\Service\Attribute\Required;
 
 class TleRequest extends AbstractRequest
 {
     public const EXTRA_PARAM = 'extra';
 
+    protected TleRepository $repository;
+
     use TleRequestTrait {
         validate as validateExtraParam;
+    }
+
+    #[Required]
+    public function setRepository(TleRepository $repository): void
+    {
+        $this->repository = $repository;
     }
 
     public function getId(): int
