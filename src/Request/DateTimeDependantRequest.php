d66147da6e61dfd5c92dfd1b02a73f71c00e1029
ivanstan <istanojevic@itekako.com>
2026-01-31 19:31:17 +0100

    Upgrade tle-php to 2.1

--- src/Request/DateTimeDependantRequest.php
+++ src/Request/DateTimeDependantRequest.php
@@ -3,6 +3,7 @@
 namespace App\Request;
 
 use Ivanstan\SymfonySupport\Services\DateTimeService;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 trait DateTimeDependantRequest
 {
@@ -10,6 +11,12 @@ trait DateTimeDependantRequest
     {
         $date = $this->get($name, DateTimeService::getCurrentUTC()->format(\DateTimeInterface::ATOM));
 
-        return \DateTime::createFromFormat(\DateTimeInterface::ATOM, str_replace(' ', '+', $date));
+        $dateTime = \DateTime::createFromFormat(\DateTimeInterface::ATOM, str_replace(' ', '+', $date));
+        
+        if ($dateTime === false) {
+            throw new BadRequestHttpException(\sprintf('Invalid date format for parameter "%s". Expected ATOM format (e.g., 2024-01-31T12:00:00+00:00), got: %s', $name, $date));
+        }
+
+        return $dateTime;
     }
 }
